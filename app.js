33a7b48d7a0903a0f9655d2fb3ebf9fcc8396f2d
hossam-khalaf <hossam.khalaf@med.au.edu.eg>
2020-10-21 01:33:37 +0200

    custom icon and refactor

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // leaflet map config
 const mymap = L.map('issMap').setView([0, 0], 1);
-const marker = L.marker([0, 0]).addTo(mymap);
+// config tiles
 const attribution =
   '&copy; <a href="https://www.openstreetmap.org/copyright">osm</a> contributors';
 const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
@@ -8,9 +8,18 @@ const tiles = L.tileLayer(tileUrl, { attribution });
 
 tiles.addTo(mymap);
 
+// config icon and marker
+const myIcon = L.icon({
+  iconUrl: 'iss.png',
+  iconSize: [22, 22],
+  iconAnchor: [11, 11],
+});
+
+const marker = L.marker([0, 0], { icon: myIcon }).addTo(mymap);
+
 //Where is ISS now api fetch data
 const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
-getISS();
+
 async function getISS() {
   const response = await fetch(api_url);
   const data = await response.json();
@@ -24,3 +33,5 @@ async function getISS() {
   document.querySelector('.lat').textContent = latitude;
   document.querySelector('.lon').textContent = longitude;
 }
+
+getISS();
