eb2fbc7f9573d2cecf2fd377b8f5650b21c28d0a
hossam-khalaf <hossam.khalaf@med.au.edu.eg>
2020-10-21 02:36:00 +0200

    change style upon time

--- app.js
+++ app.js
@@ -11,8 +11,9 @@ tiles.addTo(mymap);
 // config icon and marker
 const myIcon = L.icon({
   iconUrl: 'iss.png',
-  iconSize: [22, 22],
-  iconAnchor: [11, 11],
+  iconSize: [50, 50],
+  iconAnchor: [25, 25],
+  shadowSize: [100, 95],
 });
 
 const marker = L.marker([0, 0], { icon: myIcon }).addTo(mymap);
@@ -20,18 +21,32 @@ const marker = L.marker([0, 0], { icon: myIcon }).addTo(mymap);
 //Where is ISS now api fetch data
 const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
 
+let firstTime = true;
+
 async function getISS() {
   const response = await fetch(api_url);
   const data = await response.json();
-  const { latitude, longitude } = data;
+  const { latitude, longitude, visibility } = data;
 
   // adding marker
   marker.setLatLng([latitude, longitude]);
-  // console.log(latitude, longitude);
 
+  // set the view of map to current lon/lat
+  if (firstTime) {
+    mymap.setView([latitude, longitude], 3);
+    firstTime = false;
+  }
+
+  // change style upon the time
+  if (visibility == 'daylight') {
+    document.body.style.backgroundColor = '#222';
+  } else {
+    document.body.style.backgroundColor = '#333';
+    document.body.style.color = '#eee';
+  }
   // mainpulate data to the html
   document.querySelector('.lat').textContent = latitude;
   document.querySelector('.lon').textContent = longitude;
 }
 
-getISS();
+setInterval(getISS, 1000);
